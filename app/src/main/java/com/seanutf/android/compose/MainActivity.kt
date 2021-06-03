4123814f0a2c7ea00b920a24d99ed9923f4fc5d2
seanutf <seanutf@gmail.com>
2021-06-03 21:25:47 +0800

    init project commit

---
+++ app/src/main/java/com/seanutf/android/compose/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.seanutf.android.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.seanutf.android.compose.ui.theme.ComposeApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeApplicationTheme {
+        Greeting("Android")
+    }
+}
