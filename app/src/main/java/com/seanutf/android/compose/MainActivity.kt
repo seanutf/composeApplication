35060e25aa9f9fb7eef75ad4a9d5f038662652fc
seanutf <seanutf@gmail.com>
2021-06-04 11:47:29 +0800

    branch level0 commit

--- app/src/main/java/com/seanutf/android/compose/MainActivity.kt
+++ app/src/main/java/com/seanutf/android/compose/MainActivity.kt
@@ -3,11 +3,20 @@ package com.seanutf.android.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.seanutf.android.compose.ui.theme.ComposeApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +32,6 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
@@ -33,6 +41,31 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     ComposeApplicationTheme {
-        Greeting("Android")
+        //Greeting("Android")
+        NewsStory()
+    }
+}
+
+@Composable
+fun NewsStory() {
+    Column(
+        modifier = Modifier.padding(horizontal = 16.dp,vertical = 16.dp),
+    ){
+        Image(
+            painter = painterResource(R.drawable.header),
+            contentDescription = null,
+            modifier = Modifier.height(180.dp).fillMaxWidth().
+            clip(shape = RoundedCornerShape(4.dp)),
+            contentScale = ContentScale.Crop
+        )
+
+        Spacer(Modifier.height(16.dp))
+        Text("这是一段中文的测试文案，用于测试文案这是一段中文的测试文案，用于测试文案这是一段中文的测试文案，用于测试文案这是一段中文的测试文案，用于测试文案这是一段中文的测试文案，用于测试文案这是一段中文的测试文案，用于测试文案"
+            ,
+            style = MaterialTheme.typography.h6,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis)
+        Text("Davenport, California")
+        Text("December 2018")
     }
 }
